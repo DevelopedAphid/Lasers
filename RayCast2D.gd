4543a503a4fccf6d904bf57729c6b07bcba81488
Michael <michael.john.davis.1@gmail.com>
2020-09-27 15:53:35 +1300

    1 min laser tutorial
    Adds laser that is enabled by mouse click

--- RayCast2D.gd
+++ RayCast2D.gd
@@ -6,21 +6,30 @@ func _unhandled_input(event: InputEvent) -> void:
 	if event is InputEventMouseButton:
 		self.is_casting = event.pressed
 
-func _physics_process(delta):
+func _physics_process(_delta):
 	var cast_point := cast_to
 	force_raycast_update()
+	
+	$CollisionParticle.emitting = is_colliding()
 	
 	if is_colliding():
 		cast_point = to_local(get_collision_point())
 	
 	$Line2D.points[1] = cast_point
+	$BeamParticle.position = cast_point * 0.5
+	$BeamParticle.process_material.emission_box_extents.x = cast_point.length() * 0.5
+	$BeamParticle.process_material.emission_box_extents.y = cast_point.length() * 0.5
 
 func set_is_casting(cast: bool) -> void:
 	is_casting = cast
 	
+	$BeamParticle.emitting = is_casting
+	$CastingParticle.emitting = is_casting
+	
 	if is_casting:
 		appear()
 	else:
+		$CollisionParticle.emitting = false
 		disappear()
 	
 	set_physics_process(is_casting)
