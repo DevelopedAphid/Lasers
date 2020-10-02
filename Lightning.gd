d3993ab81a73103f32c6d4a8caac4c633eddc5ae
Michael <michael.john.davis.1@gmail.com>
2020-10-02 19:56:20 +1300

    Fix 90 degree lightning not having spread
    - Frees memory on removing all points of the lightning.
    - Refactor Lightning direction so that aiming along 90degree angles still has spread to the lightning

--- Lightning.gd
+++ Lightning.gd
@@ -1,10 +1,12 @@
 extends Node2D
 
+#TODO - add collision object so that we can zap things
+
 var is_casting = false
 var count = 0 
 var point_count = 0
-var random_number_1
-var random_number_2
+var random_number_x
+var random_number_y
 var lightning_direction
 
 func _ready():
@@ -18,7 +20,7 @@ func _ready():
 
 func _physics_process(_delta):
 	if is_casting:
-		if point_count > 10:
+		if point_count > 15:
 			is_casting = false
 		
 		elif count < 2:
@@ -26,11 +28,14 @@ func _physics_process(_delta):
 		
 		elif count == 2:
 			randomize()
-			random_number_1 = rand_range(-15,50)
-			random_number_2 = rand_range(-15,50)
-		
+			random_number_x = rand_range(-5,50)
+			random_number_y = rand_range(-5,50)
+			var spread = rand_range(-0.5,0.5)
+			
 			var last_position = $Line2D.points[point_count]
-			$Line2D.add_point(last_position + lightning_direction * Vector2(random_number_1,random_number_2))
+			var next_x = lightning_direction.x * random_number_x + spread * random_number_x 
+			var next_y = lightning_direction.y * random_number_y + spread * random_number_y
+			$Line2D.add_point(last_position + Vector2(next_x, next_y))
 			point_count += 1
 		
 		elif count < 2:
@@ -40,3 +45,4 @@ func _physics_process(_delta):
 		for n in range(point_count,0,-1):
 			$Line2D.remove_point(n)
 		point_count = 0
+		queue_free()
